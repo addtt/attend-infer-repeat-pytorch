d97dce342afd8c0f2e0c287e9fe348e7320a0931
Andrea Dittadi <andrea.dittadi@gmail.com>
2020-01-17 11:28:56 +0100

    Update version check

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ from boilr import Trainer
 from experiments import AIRExperiment
 
 
-def _check_version(pkg, pkg_str, version):
-    if pkg.__version_info__[:2] != version[:2]:
-        msg = "This was last tested with {} version {}, but the current version is {}"
-        msg = msg.format(pkg_str, version, pkg.__version_info__)
+def _check_version(pkg, pkg_str, version_info):
+    def to_str(v):
+        return ".".join(str(x) for x in v)
+    if pkg.__version_info__[:2] != version_info[:2]:
+        msg = "This was last tested with {} {}, but the current version is {}"
+        msg = msg.format(pkg_str, to_str(version_info), pkg.__version__)
         warnings.warn(msg)
 
-BOILR_VERSION = (0, 3, 5)
+BOILR_VERSION = (0, 4, 0)
 MULTIOBJ_VERSION = (0, 0, 3)
 _check_version(boilr, 'boilr', BOILR_VERSION)
 _check_version(multiobject, 'multiobject', MULTIOBJ_VERSION)
